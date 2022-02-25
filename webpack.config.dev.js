d5fe41071e42bc2394fc46556cd0e91869f80b11
diegoherrerach <herrera.ch.diego@gmail.com>
2022-02-25 12:14:25 -0500

    Add webpack service

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 module.exports ={
     entry: './src/index.js',
@@ -12,7 +13,7 @@ module.exports ={
         assetModuleFilename:'assets/images/[hash][ext][query]'
     },
     mode:'development',
-    watch:true,
+    devtool: 'source-map',
     resolve:{
         extensions: ['.js'],
         alias:{
@@ -73,7 +74,20 @@ module.exports ={
         ]
         }),
         new Dotenv(),
+        new BundleAnalyzerPlugin(),
     ],
+    devServer:{
+        static: 
+         {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        },
+        watchFiles: path.join(__dirname, "./**"),
+        compress: true,
+        historyApiFallback: true,
+        port:3006,
+        open:true,
+    },
 
 }
 
